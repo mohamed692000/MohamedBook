6c7621b74f05829910fe2792e27129ff9f7f716c
mohamed692000 <58400177+mohamed692000@users.noreply.github.com>
2023-11-25 01:38:00 +0200

    hello mo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MohamedBook.Models;
+
+namespace MohamedBook.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
